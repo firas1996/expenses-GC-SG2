0810240068888147f1c896ef0190b97988441b42
firas hamila <firas.hm.1996@gmail.com>
2025-02-14 11:08:11 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import React from "react";
-import MyFrag from "./UI/MyFrag";
 import ExpenseDetails from "./components/ExpenseDetails";
 
 function App() {
-  return <ExpenseDetails />;
+  const expensesData = [
+    {
+      id: 1,
+      title: "Voyage",
+      price: 350,
+      date: new Date("2025-02-14"),
+    },
+    {
+      id: 2,
+      title: "Shopping",
+      price: 150,
+      date: new Date("2024-10-18"),
+    },
+    {
+      id: 3,
+      title: "Car",
+      price: 100,
+      date: new Date("2025-05-14"),
+    },
+    {
+      id: 4,
+      title: "Education",
+      price: 300,
+      date: new Date("2023-10-14"),
+    },
+  ];
+  return (
+    <div>
+      {expensesData.map((expense) => {
+        return (
+          <ExpenseDetails
+            title={expense.title}
+            price={expense.price}
+            date={expense.date}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;

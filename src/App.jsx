e732544054c7b8c6bba9f18917cf63ec228967d3
firas hamila <firas.hm.1996@gmail.com>
2025-02-14 10:41:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MyFrag from "./UI/MyFrag";
+import ExpenseDetails from "./components/ExpenseDetails";
+
+function App() {
+  return <ExpenseDetails />;
+}
+
+export default App;
---
+++ src/components/ExpenseDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseDetails = () => {
+  return (
+    <div className="expense-item">
+      <div className="expense-date">date</div>
+      <div className="expense-item__description">
+        <h2>title</h2>
+        <div className="expense-item__price">$ price</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseDetails;

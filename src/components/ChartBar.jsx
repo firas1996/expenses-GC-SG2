dce9a78d76ae586ce7a019a09f3b10f0ed3eb7a8
firas hamila <firas.hm.1996@gmail.com>
2025-02-28 11:46:19 +0100

    a

--- src/components/ChartBar.jsx
+++ src/components/ChartBar.jsx
@@ -1,10 +1,11 @@
 import "./ChartBar.css";
 
-const ChartBar = ({ name }) => {
+const ChartBar = ({ name, value, max, total }) => {
+  const progress = (value / max) * 100 + "%";
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill"></div>
+        <div className="chart-bar__fill" style={{ height: progress }}></div>
       </div>
       <h5 className="chart-bar__label">{name}</h5>
     </div>

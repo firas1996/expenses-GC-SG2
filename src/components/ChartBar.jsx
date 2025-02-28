3ad6cf79771c99f2d69d6464b1b2787e14a1b985
firas hamila <firas.hm.1996@gmail.com>
2025-02-28 11:04:46 +0100

    a

--- src/components/ChartBar.jsx
+++ src/components/ChartBar.jsx
@@ -1,12 +1,12 @@
 import "./ChartBar.css";
 
-const ChartBar = () => {
+const ChartBar = ({ name }) => {
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
         <div className="chart-bar__fill"></div>
       </div>
-      <h5 className="chart-bar__label">Jan</h5>
+      <h5 className="chart-bar__label">{name}</h5>
     </div>
   );
 };
